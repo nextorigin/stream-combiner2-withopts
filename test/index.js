095b866900d32ecee8889462bd2825c444c48ebd
Charles Phillips <charles@doublerebel.com>
2016-08-11 20:57:56 -0700

    [test] Merge

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 var es = require('event-stream')
 var through = require('through2')
 var combine = require('..')
+var merge = combine.Merge
 var test = require('tape')
 
 test('re-emit error object for old streams', function (test) {
@@ -140,6 +141,129 @@ test('unwrap unbinds streams', function (test) {
   pipe.emit("finish")
   try {
     pipe.write('meh')
+  } catch (err) {
+    test.ok(err.toString().match(/meh/), "should have unbound error handlers")
+    test.notOk(errors, "expected no errors")
+  }
+})
+
+test('Merge: re-emit error object for old streams', function (test) {
+  test.plan(1)
+
+  var expectedErr = new Error('asplode')
+
+  var pipe = merge(
+    es.through(function(data) {
+      return this.emit('error', expectedErr)
+    })
+  )
+
+  pipe.on('error', function (err) {
+    test.equal(err, expectedErr)
+  })
+
+  pipe.write('pow')
+})
+
+test('Merge: 3 pipe merge data', function (test) {
+  test.plan(3)
+  var errors = 0;
+  var source1 = through()
+  var source2 = through()
+  var source3 = through()
+  var pipe = merge(
+    source1,
+    source2,
+    source3
+  )
+   , expected = [ 'beep', 'boop', 'robots' ]
+
+  pipe.on("data", function(data) {
+    test.equal(data.toString('utf8'), expected.shift())
+  })
+
+  source3.write('beep')
+  source2.write('boop')
+  source1.end('robots')
+})
+
+test('Merge: 0 argument through stream', function (test) {
+  test.plan(3)
+  var pipe = merge()
+   , expected = [ 'beep', 'boop', 'robots' ]
+
+  pipe.pipe(es.through(function(data) {
+    test.equal(data.toString('utf8'), expected.shift())
+  }))
+  pipe.write('beep')
+  pipe.write('boop')
+  pipe.end('robots')
+})
+
+test('Merge: object mode', function (test) {
+  test.plan(2)
+  var opts = {objectMode: true}
+   , expected = [ [4,5,6], {x:5} ]
+   , source = through.obj()
+
+  var pipe = merge(
+    es.through(),
+    source,
+    opts
+  )
+  pipe.on("data", function(data, enc, next) {
+    test.deepEqual(data, expected.shift())
+  })
+  source.write([4,5,6])
+  source.write({x:5})
+  pipe.end()
+})
+
+test('Merge: unwrap unpipes streams', function (test) {
+  test.plan(1)
+  var errors = 0;
+  var source2 = es.through()
+  var pipe = merge(
+    es.through(),
+    source2
+  )
+  pipe.on("data", function(data) {
+    errors++
+    return test.fail("should not have piped")
+  })
+
+  pipe.unwrap()
+  source2.write('meh')
+  test.notOk(errors, "expected no errors")
+})
+
+test('Merge: unwrap unbinds streams', function (test) {
+  test.plan(2)
+  var errors = 0;
+  var source1 = es.through(function(data) {
+    return this.emit("error", new Error(data))
+  })
+  var source2 = es.through()
+  var pipe = merge(
+    source1,
+    source2,
+    es.through()
+  )
+
+  pipe.on('error', function(err) {
+    errors++
+    return test.fail("should not have bubbled error")
+  })
+
+  pipe.on("data", function() {
+    errors++
+    return test.fail("should not have piped")
+  })
+
+  pipe.unwrap()
+  try {
+    source2.write('meh')
+    source1.write('meh')
   } catch (err) {
     test.ok(err.toString().match(/meh/), "should have unbound error handlers")
     test.notOk(errors, "expected no errors")
